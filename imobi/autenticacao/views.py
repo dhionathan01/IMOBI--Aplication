32bd902faa5692bbf43ff3159ca56df16ed011a4
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-01-12 19:06:35 -0300

    Cadastro de usuários

--- imobi/autenticacao/views.py
+++ imobi/autenticacao/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+
+
 # Create your views here.
 
 
@@ -10,7 +15,19 @@ def cadastro(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
-        return HttpResponse(f'{username} {email} {senha}')
+        if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            return redirect('/auth/cadastro')
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            return redirect('/auth/cadastro')
+        try:
+            user = User(username=username, email=email, password=senha)
+
+            user.save()
+            return redirect('/auth/logar')
+        except:
+            return redirect('/auth/cadastro')
 
 
 def logar(request):
