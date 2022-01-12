fd31fe089089500f4cfe86885ceab7f2ce97cad0
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-01-12 19:13:16 -0300

    Exibindo mensagens de erro e sucesso de cadastro

--- imobi/autenticacao/views.py
+++ imobi/autenticacao/views.py
@@ -16,17 +16,20 @@ def cadastro(request):
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
             return redirect('/auth/cadastro')
         user = User.objects.filter(username=username)
 
         if user.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse nome cadastrado')
             return redirect('/auth/cadastro')
         try:
             user = User(username=username, email=email, password=senha)
-
             user.save()
+            messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso')
             return redirect('/auth/logar')
         except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
             return redirect('/auth/cadastro')
 
 
