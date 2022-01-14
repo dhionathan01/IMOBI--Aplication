382b24b285126debff5da8059feb65a850133294
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-01-14 16:28:26 -0300

    Criando e tornando funcional o sistema de autenticação

--- imobi/autenticacao/views.py
+++ imobi/autenticacao/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
-
+from django.contrib import auth
 
 # Create your views here.
 
 
 def cadastro(request):
     if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'cadastro.html')
     elif request.method == "POST":
         username = request.POST.get('username')
@@ -24,7 +26,7 @@ def cadastro(request):
             messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse nome cadastrado')
             return redirect('/auth/cadastro')
         try:
-            user = User(username=username, email=email, password=senha)
+            user = User.objects.create_user(username=username, email=email, password=senha)
             user.save()
             messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso')
             return redirect('/auth/logar')
@@ -34,4 +36,17 @@ def cadastro(request):
 
 
 def logar(request):
-    return render(request, 'logar.html')
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'logar.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        usuario = auth.authenticate(username=username, password=senha)
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Usuário não existe')
+            return redirect('/auth/logar')
+        else:
+            auth.login(request, usuario)
+            return redirect('/')
