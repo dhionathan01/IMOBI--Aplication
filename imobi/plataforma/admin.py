a7ab90e906036e211ffb95d235c4f198d1fca581
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-01-21 15:05:48 -0300

    Personalizando a página admin django

--- imobi/plataforma/admin.py
+++ imobi/plataforma/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import DiasVisita, Cidade, Imagem, Horario, Imovel, Visitas
 
-# Register your models here.
+
+@admin.register(Imovel)
+class ImoveiAdmin(admin.ModelAdmin):
+    list_display = ('rua', 'valor', 'quartos', 'tamanho', 'cidade', 'tipo')
+    list_editable = ('valor', 'tipo')
+    list_filter = ('cidade', 'tipo')
+
+
+admin.site.register(DiasVisita)
+admin.site.register(Cidade)
+admin.site.register(Imagem)
+admin.site.register(Horario)
+admin.site.register(Visitas)
