22082b98560ca0e7d6dad50a4927297e169b9715
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-01-21 16:22:29 -0300

    Criando página home, e pegando as informações do banco de dados

--- imobi/plataforma/views.py
+++ imobi/plataforma/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.decorators import  login_required
+from django.contrib.auth.decorators import login_required
+from .models import Imovel, Cidade
 
 
 @login_required(login_url='/auth/logar')
 def home(request):
-    return HttpResponse('Estou na Home')
+    preco_minimo = request.GET.get('preco_minimo')
+    preco_maximo = request.GET.get('preco_maximo')
+    cidade = request.GET.get('cidade')
+    tipo = request.GET.getlist('tipo')
+    imoveis = Imovel.objects.all()
+    cidades = Cidade.objects.all()
+    print(imoveis)
+    if preco_minimo or preco_maximo or cidade or tipo:
+
+        if not preco_minimo:
+            preco_minimo = 0
+        if not preco_maximo:
+            preco_maximo = 999999999
+        if not tipo:
+            tipo = ['A', 'C']
+
+        imoveis = Imovel.objects.filter(valor__gte=preco_minimo) \
+            .filter(valor__lte=preco_maximo) \
+            .filter(tipo_imovel__in=tipo).filter(cidade=cidade)
+    else:
+        imoveis = Imovel.objects.all()
+    return render(request, 'home.html', {'imoveis': imoveis, 'cidades': cidades})
